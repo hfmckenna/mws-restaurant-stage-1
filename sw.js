a0cbb5fcfcf418fa0305eda3d42934a4ba00fcb0
Hugh McKenna <hf.mckenna@gmail.com>
2018-07-15 10:27:13 +0100

    fix: Service worker ignores url query parameter, aria roles added

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request,{ignoreSearch: true}).then(function(response) {
         return response || fetch(event.request).then(function(response) {
           return caches.open(CACHE_NAME).then(function(cache) {
             cache.put(event.request, response.clone());
